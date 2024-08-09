629a59e3462567671498acd22edb61a097303c3c
Destiny Moncrief <D.moncrief4@gmail.com>
2024-08-09 01:22:02 -0400

    login and registration form completed

--- scripts/login.js
+++ scripts/login.js
@@ -1,42 +1,14 @@
 "use strict"
 
 
-const loginBtn = document.querySelectorAll(" .login-btn")
-const registerBtn = document.querySelectorAll(".register-btn")
-const lostPassBtn = document.querySelectorAll(".lost-pass-btn");
- const box = document.querySelector(".box")
- const loginForm = document.querySelector(".register-form")
- const lostPasswordForm = document.querySelector( ".lost-password-form");
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-
- registerBtn.forEach((btn) =>{
-    btn.addEventListener("Click",() => {
-        box.classList.add("slide-active");
-        registerForm.classList.remove("form-hidden");
-        loginForm.classList.add("form-hidden");
-        lostPasswordForm.classList.add("form-hidden");
-        
-    });
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
 });
 
-
-loginBtn.forEach((btn) =>{
-    btn.addEventListener("Click",() => {
-        box.classList.remove("slide-active");
-        registerForm.classList.add("form-hidden");
-        loginForm.classList.remove("form-hidden");
-        lostPasswordForm.classList.add("form-hidden");
-        
-    });
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
 });
-
-
-lostPassBtn.forEach((btn) =>{
-    btn.addEventListener("Click",() => {
-        registerForm.classList.remove("form-hidden");
-        loginForm.classList.add("form-hidden");
-        lostPasswordForm.classList.remove("form-hidden");
-        
-    });
-});
-
